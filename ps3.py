5bb575dc81f4c9153b9df9fe66158160402cba40
Brant Shapka <bshapka@gmail.com>
2017-09-04 00:01:22 -0700

    implemented get_word_score

--- ps3.py
+++ ps3.py
@@ -16,7 +16,9 @@ CONSONANTS = 'bcdfghjklmnpqrstvwxyz'
 HAND_SIZE = 7
 
 SCRABBLE_LETTER_VALUES = {
-    'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
+    'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 
+    'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 
+    'w': 4, 'x': 8, 'y': 4, 'z': 10
 }
 
 # -----------------------------------
@@ -92,7 +94,11 @@ def get_word_score(word, n):
     returns: int >= 0
     """
     
-    pass  # TO DO... Remove this line when you implement this function
+    letter_points = sum([SCRABBLE_LETTER_VALUES[letter.lower()] for letter in word])
+    word_length_points = max(1, 7 * len(word) - 3 * (n - len(word)))
+    return letter_points * word_length_points
+
+
 
 #
 # Make sure you understand how this function works and what it does!
