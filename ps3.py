9e499c130166215eb0ddb2f2c0f0219522e401c6
Brant Shapka <bshapka@gmail.com>
2017-09-04 16:30:56 -0700

    removed small changes made for wildcards problem

--- ps3.py
+++ ps3.py
@@ -18,7 +18,7 @@ HAND_SIZE = 7
 SCRABBLE_LETTER_VALUES = {
     'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 
     'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 
-    'w': 4, 'x': 8, 'y': 4, 'z': 10, '*': 0
+    'w': 4, 'x': 8, 'y': 4, 'z': 10
 }
 
 # -----------------------------------
@@ -140,9 +140,7 @@ def deal_hand(n):
     """
     
     hand={}
-    num_vowels = int(math.ceil(n / 3)) - 1
-    
-    hand['*'] = 1
+    num_vowels = int(math.ceil(n / 3))
 
     for i in range(num_vowels):
         x = random.choice(VOWELS)
